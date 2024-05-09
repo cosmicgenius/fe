76dc23a6cad06ff9c80b981baf7b83f2d2671110
Anthony Wang <anthonyywang05@gmail.com>
2024-05-09 15:51:51 -0400

    Filter out non FEs

--- gen/filter_data.py
+++ gen/filter_data.py
@@ -18,28 +18,34 @@ def main():
     
     print(f"Read {len(gen)} lines of clean data")
 
+    gen = gen[:10000]
+
     filtered = set()
     t0 = time.time()
+    l0 = 0
     for i, fe in enumerate(gen):
-        proc = Popen([build_path, "--pretty=false", "--simp=2", "--simp_timeout=800"], 
+        proc = Popen([build_path, "--pretty=false", "--simp=2", "--simp_timeout=750"], 
                      encoding="utf-8", stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
         try:
-            stdout, _ = proc.communicate(f"h {fe}\ne\n", timeout=1)
+            stdout, _ = proc.communicate(f"h {fe}\ne\n", timeout=2)
             stats = [tuple(int(s) for s in m) for m in stat_regex.findall(stdout)]
 
-            # must parse and have no solution
-            # of the form f(a x + b) = stuff with no f's
-            if len(stats) > 0 and all(s[1] != 2 for s in stats):
+            # must 
+            # 1) parse 
+            # 2) have no solution of the form f(a x + b) = stuff with no f's
+            # 3) have at least one f(x)
+            if len(stats) > 0 and all(s[1] != 2 for s in stats) and not all(s[1] == 0 for s in stats):
                 filtered.add(fe)
 
         except TimeoutExpired:
-            print(f"Number {i} taking too long; killed after 1s")
+            print(f"Number {i} taking too long; killed after 2s")
             proc.kill()
             stdout, _ = proc.communicate()
 
         if (i + 1) % eval_period == 0:
-            print(f"Line {i + 1}: last {eval_period} lines took {time.time() - t0:.3f}s")
+            print(f"Line {i + 1}: last {eval_period} lines took {time.time() - t0:.3f}s and got {len(filtered) - l0} results")
             t0 = time.time()
+            l0 = len(filtered)
 
     filtered = list(filtered)
 
