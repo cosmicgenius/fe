515548026ca0e721a8c34bd9ff657510121f973b
Anthony Wang <anthonyywang05@gmail.com>
2024-05-09 13:04:14 -0400

    Add more accurate filtering

--- gen/filter_data.py
+++ gen/filter_data.py
@@ -4,11 +4,12 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")) # Stupid python
 
 import re
+import time
 from subprocess import Popen, PIPE, TimeoutExpired, DEVNULL
 from config import build_path, gen_path, filtered_path
 
-wregex = re.compile(r'\[weight=(\d+)]')
-timeout = 1
+stat_regex = re.compile(r'\[w=(\d+),nw=(\d+),d=(\d+),la=(\d+)\]')
+eval_period = 100
 
 def main():
     gen = []
@@ -18,22 +19,27 @@ def main():
     print(f"Read {len(gen)} lines of clean data")
 
     filtered = set()
+    t0 = time.time()
     for i, fe in enumerate(gen):
-        proc = Popen([build_path, "--pretty=false", "--simp=2"], 
+        proc = Popen([build_path, "--pretty=false", "--simp=2", "--simp_timeout=800"], 
                      encoding="utf-8", stdin=PIPE, stdout=PIPE, stderr=DEVNULL)
         try:
-            stdout, _ = proc.communicate(f"h {fe}\ne\n", timeout=timeout)
-            weights = [int(w) for w in wregex.findall(stdout)]
+            stdout, _ = proc.communicate(f"h {fe}\ne\n", timeout=1)
+            stats = [tuple(int(s) for s in m) for m in stat_regex.findall(stdout)]
 
-            # must parse and have no linear solution
-            # of the form a f(x) + b x = c where a,b,c are constants 
-            if len(weights) > 0 and all(w == 0 or w > 6 for w in weights):
+            # must parse and have no solution
+            # of the form f(a x + b) = stuff with no f's
+            if len(stats) > 0 and all(s[1] != 2 for s in stats):
                 filtered.add(fe)
 
         except TimeoutExpired:
-            print(f"Number {i} taking too long; killed after {timeout}s")
+            print(f"Number {i} taking too long; killed after 1s")
             proc.kill()
             stdout, _ = proc.communicate()
+
+        if (i + 1) % eval_period == 0:
+            print(f"Line {i + 1}: last {eval_period} lines took {time.time() - t0:.3f}s")
+            t0 = time.time()
 
     filtered = list(filtered)
 
